a10b3d8ac7b9e94c18b9722363256e89489457bc
Alimov Tillo <alimovtillo@gmail.com>
2022-05-08 23:54:15 +0600

    ff

--- project/core/urls.py
+++ project/core/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from core import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,4 +23,7 @@ from apps.api.urls import urlpatterns as news_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(news_urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
